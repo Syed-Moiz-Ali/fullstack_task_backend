d962d2d08185a0acdd8ec72461b9b24a56a378cb
Syed Moiz Ali <100577359+Syed-Moiz-Ali@users.noreply.github.com>
2024-05-30 00:39:29 +0530

    Update index.js

--- index.js
+++ index.js
@@ -40,75 +40,78 @@ async function startServer() {
     const usersCollection = db.collection("users");
     const squareStatesCollection = db.collection("squareStates");
 
-    // User registration endpoint
-    app.post("/api/signup", async (req, res) => {
-      const { email, password } = req.body;
-      const hashedPassword = await bcrypt.hash(password, 10);
-      const user = new User(email, hashedPassword);
-      await usersCollection.insertOne(user);
-      res.send({ message: "Sign up successful" });
-    });
-
-    // User login endpoint
-    app.post("/api/login", async (req, res) => {
-      const { email, password } = req.body;
-      const user = await usersCollection.findOne({ email });
-      if (user && (await bcrypt.compare(password, user.password))) {
-        const token = jwt.sign({ email: user.email }, SECRET_KEY, {
-          expiresIn: "1h",
-        });
-        res.send({ token });
-      } else {
-        res.status(401).send({ message: "Invalid credentials" });
-      }
-    });
-
-    // Middleware to authenticate JWT token
-    const authenticateJWT = (req, res, next) => {
-      const token = req.headers.authorization;
-      if (token) {
-        jwt.verify(token, SECRET_KEY, (err, user) => {
-          if (err) {
-            return res.sendStatus(403);
-          }
-          req.user = user;
-          next();
-        });
-      } else {
-        res.sendStatus(401);
-      }
-    };
-
-    // Endpoint to receive the current state of squares from the frontend
-    app.get("/api/squares", authenticateJWT, async (req, res) => {
-      const state = await squareStatesCollection.findOne({});
-      res.send(state ? state.squares : []);
-    });
-
-    // Endpoint to update the state of squares when they are all white
-    app.post("/api/squares", authenticateJWT, async (req, res) => {
-      const { squares } = req.body;
-      let state = await squareStatesCollection.findOne({});
-      if (!state) {
-        state = new SquareState(squares);
-        await squareStatesCollection.insertOne(state);
-      } else {
-        await squareStatesCollection.updateOne(
-          { _id: state._id },
-          { $set: { squares: squares } }
-        );
-      }
-      res.send({ message: "State updated successfully" });
-    });
-
-    app.listen(PORT, () => {
-      console.log(`Server is running on http://localhost:${PORT}`);
-    });
+  
   } catch (error) {
     console.error("Error starting the server:", error);
     process.exit(1);
   }
 }
 
+
 startServer();
-module.exports = app;
+
+  // User registration endpoint
+  app.post("/api/signup", async (req, res) => {
+    const { email, password } = req.body;
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const user = new User(email, hashedPassword);
+    await usersCollection.insertOne(user);
+    res.send({ message: "Sign up successful" });
+  });
+
+  // User login endpoint
+  app.post("/api/login", async (req, res) => {
+    const { email, password } = req.body;
+    const user = await usersCollection.findOne({ email });
+    if (user && (await bcrypt.compare(password, user.password))) {
+      const token = jwt.sign({ email: user.email }, SECRET_KEY, {
+        expiresIn: "1h",
+      });
+      res.send({ token });
+    } else {
+      res.status(401).send({ message: "Invalid credentials" });
+    }
+  });
+
+  // Middleware to authenticate JWT token
+  const authenticateJWT = (req, res, next) => {
+    const token = req.headers.authorization;
+    if (token) {
+      jwt.verify(token, SECRET_KEY, (err, user) => {
+        if (err) {
+          return res.sendStatus(403);
+        }
+        req.user = user;
+        next();
+      });
+    } else {
+      res.sendStatus(401);
+    }
+  };
+
+  // Endpoint to receive the current state of squares from the frontend
+  app.get("/api/squares", authenticateJWT, async (req, res) => {
+    const state = await squareStatesCollection.findOne({});
+    res.send(state ? state.squares : []);
+  });
+
+  // Endpoint to update the state of squares when they are all white
+  app.post("/api/squares", authenticateJWT, async (req, res) => {
+    const { squares } = req.body;
+    let state = await squareStatesCollection.findOne({});
+    if (!state) {
+      state = new SquareState(squares);
+      await squareStatesCollection.insertOne(state);
+    } else {
+      await squareStatesCollection.updateOne(
+        { _id: state._id },
+        { $set: { squares: squares } }
+      );
+    }
+    res.send({ message: "State updated successfully" });
+  });
+
+  app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+  });
+
